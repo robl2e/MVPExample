ff19c1fa276b0e82a47d837d0d118150218617d1
robl2e <robl2e@users.noreply.github.com>
2020-07-19 14:54:49 -0700

    Updates for androidx migration and use android 28 built tool versions.

--- app/src/main/java/com/robl2e/mvpexample/ui/main/MainActivity.java
+++ app/src/main/java/com/robl2e/mvpexample/ui/main/MainActivity.java
@@ -1,7 +1,7 @@
 package com.robl2e.mvpexample.ui.main;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
